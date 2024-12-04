619ed941196c5b66e70ab04409ee1452ad76de19
Rémi Bastide <Remi.Bastide@gmail.com>
2024-12-04 09:35:30 +0100

    dev 2 a modifié

--- src/main/java/org/github/bastide/Main.java
+++ src/main/java/org/github/bastide/Main.java
@@ -3,6 +3,7 @@ package org.github.bastide;
 public class Main {
     public static void main(String[] args) {
         // Un commentaire
+        // modif à gaucche
         System.out.println("Hello, World!");
 
     }
